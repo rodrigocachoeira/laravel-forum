5da7ecd55bb81d3075d63733b0f0f3aef1e1b0ca
Rodrigo Cachoeira <rodrigocachoeira11@gmail.com>
2017-10-13 14:44:45 -0300

    lesson 20

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer('*', function ($view) {
-           $view->with('channels', Channel::all());
+            $channels = \Cache::rememberForever('channels', function() {
+                return Channel::all();
+            });
+           $view->with('channels', $channels);
         });
     }
 
@@ -27,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 }
